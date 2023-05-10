8fa9d3298b1926845db0321727d0db82504d1170
Mikus Kukainis <mikuskukainis@gmail.com>
2023-05-10 14:30:09 +0300

    First commit

---
+++ src/main/java/io/codelex/loops/practice/Piglet.java
@@ -0,0 +1,43 @@
+package io.codelex.loops.practice;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Piglet {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random r = new Random();
+
+        System.out.println("Welcome to Piglet!");
+        int score = 0;
+        boolean rollAgain = true;
+
+        while (rollAgain) {
+            int roll = r.nextInt(6) + 1;
+            System.out.println("You rolled a " + roll + "!");
+
+            if (roll == 1) {
+                System.out.println("You got 0 points.");
+                rollAgain = false;
+            } else {
+                score += roll;
+                System.out.println("Roll again? (yes/no)");
+
+                String nextRoll = "";
+                while (!nextRoll.equals("yes") && !nextRoll.equals("no") && !nextRoll.equals("no") && !nextRoll.equals("n")) {
+                    nextRoll = input.nextLine().toLowerCase();
+                    if (!nextRoll.equals("yes") && !nextRoll.equals("no") && !nextRoll.equals("no") && !nextRoll.equals("n")) {
+                        System.out.println("Invalid input. Please enter 'yes' or 'no'.");
+                    }
+                }
+
+                if (nextRoll.equals("no")) {
+                    System.out.println("You got " + score + " points.");
+                    rollAgain = false;
+                } else {
+                    rollAgain = true;
+                }
+            }
+        }
+    }
+}
