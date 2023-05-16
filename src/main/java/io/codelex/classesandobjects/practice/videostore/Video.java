d8037863c81a9f3a02b2332a21252a2eed3c9b3e
Mikus Kukainis <mikuskukainis@gmail.com>
2023-05-16 11:35:31 +0300

    Second commit

--- src/main/java/io/codelex/classesandobjects/practice/videostore/Video.java
+++ src/main/java/io/codelex/classesandobjects/practice/videostore/Video.java
@@ -2,45 +2,32 @@ package io.codelex.classesandobjects.practice.videostore;
 
 public class Video {
     private String title;
-    private boolean checkedOut;
-    private int averageRating;
+    private boolean checkedOut = true;
     private int totalRatings;
 
-    public Video(String title) {
+    public Video(String title, int totalRatings) {
         this.title = title;
+        this.totalRatings = totalRatings;
+    }
+
+    public void addRating(int rating) {
+        this.totalRatings = (this.totalRatings + rating) / 2;
+    }
+
+    public void setCheckedOut() {
+        checkedOut = true;
+    }
+
+    public void setCheckedIn() {
         checkedOut = false;
-        averageRating = 0;
-        totalRatings = 0;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public boolean isCheckedOut() {
-        return checkedOut;
-    }
-
-    public void checkOut() {
-        checkedOut = true;
-    }
-
-    public void returnVideo() {
-        checkedOut = false;
-    }
-
-    public int getAverageRating() {
-        return averageRating;
-    }
-
-    public void receiveRating(int rating) {
-        averageRating = (averageRating * totalRatings + rating) / (totalRatings + 1);
-        totalRatings++;
-    }
-
     @Override
     public String toString() {
-        String status = checkedOut ? "Checked out" : "On shelves";
-        return "Title: " + title + ", Average Rating: " + averageRating + ", Status: " + status;
+        return "Title" + title + " | rating: " + totalRatings + " | is available: " + checkedOut;
     }
 }
