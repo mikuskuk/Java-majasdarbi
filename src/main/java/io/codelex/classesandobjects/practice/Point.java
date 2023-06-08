cd336de14126afbc14abab5fc8d883cf1b14a50d
Mikus Kukainis <mikuskukainis@gmail.com>
2023-06-08 23:38:11 +0300

    Main methods moved to own classes

--- src/main/java/io/codelex/classesandobjects/practice/Point.java
+++ src/main/java/io/codelex/classesandobjects/practice/Point.java
@@ -1,8 +1,8 @@
 package io.codelex.classesandobjects.practice;
 
 public class Point {
-    private int x;
-    private int y;
+    int x;
+    int y;
 
     public Point(int x, int y) {
         this.x = x;
@@ -18,15 +18,5 @@ public class Point {
 
         p2.x = tempX;
         p2.y = tempY;
-    }
-
-    public static void main(String[] args) {
-        Point p1 = new Point(5, 2);
-        Point p2 = new Point(-3, 6);
-
-        swapPoints(p1, p2);
-
-        System.out.println("(" + p1.x + ", " + p1.y + ")");
-        System.out.println("(" + p2.x + ", " + p2.y + ")");
     }
 }
