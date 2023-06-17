a051e9f4d6b51ec140bc8f405397afac775bab3d
Mikus Kukainis <mikuskukainis@gmail.com>
2023-06-17 08:13:18 +0200

    First commit

---
+++ src/main/java/io/codelex/numberstriviaapp/NumbersTriviaGameApp.java
@@ -0,0 +1,10 @@
+package io.codelex.numberstriviaapp;
+
+public class NumbersTriviaGameApp {
+
+    public static void main(String[] args) {
+        NumbersTriviaGame app = new NumbersTriviaGame();
+        app.startGame();
+    }
+
+}
