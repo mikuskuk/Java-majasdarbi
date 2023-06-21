248b16817536dddf1650f55716365d4d547c38d2
Mikus Kukainis <mikuskukainis@gmail.com>
2023-06-21 10:36:51 +0300

    Fixed Credit Card and Basket

--- src/main/java/io/codelex/knowledgecheck/exercise1/DebitCard.java
+++ src/main/java/io/codelex/knowledgecheck/exercise1/DebitCard.java
@@ -1,25 +1,19 @@
 package io.codelex.knowledgecheck.exercise1;
 
+import java.math.BigDecimal;
+
 public class DebitCard extends Card {
 
-    public DebitCard(String number, String owner, String ccv, double balance) {
+    public DebitCard(String number, String owner, String ccv, BigDecimal balance) {
         super(number, owner, ccv, balance);
     }
 
     @Override
-    public void addMoney(double amount) {
-        balance += amount;
-        if (balance > 10000) {
+    public void addMoney(BigDecimal amount) {
+        balance = balance.add(amount);
+        if (balance.compareTo(BigDecimal.valueOf(10000)) > 0) {
             System.out.println("Warning: Too much money");
         }
     }
 
-    @Override
-    public void takeMoney(double amount) throws NotEnoughFundsException {
-        if (amount <= balance) {
-            balance -= amount;
-        } else {
-            throw new NotEnoughFundsException("Not enough funds!");
-        }
-    }
 }
